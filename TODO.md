fbc5e98706ddafe4e300901f296ab9c9d84712a9
kgryte <kgryte@gmail.com>
2015-04-21 18:56:26 -0700

    [UPDATE] put/post requests should return 204 no content responses.

--- TODO.md
+++ TODO.md
@@ -6,7 +6,12 @@ TODO
 3. 	watch/reload
 4. 	auto open browser?
 	-	only if UI component
-5.  update generator
+5.  
 6.  authentication for `/logs` route
 	-	or more generally, ability to toggle authentication for all routes
-7. 	
+7. 	cache of most recent logs ???
+	-	GET /logs
+	-	provide endpoint for remotely getting logs
+8. 	graceful-exit
+9. 	monitor!!!! Use compute modules.
+10. 
--- app/middleware/logs/okay.js
+++ app/middleware/logs/okay.js
@@ -2,14 +2,14 @@
 
 /**
 * FUNCTION: okay( request, response, next )
-*	Returns an `ok` (200) response.
+*	Returns a success response.
 *
 * @param {Object} request - HTTP request object
 * @param {Object} response - HTTP response object
 * @param {Function} next - callback to invoke after sending response
 */
 function okay( request, response, next ) {
-	response.sendStatus( 200 );
+	response.sendStatus( 204 );
 	next();
 } // end FUNCTION okay()
 
